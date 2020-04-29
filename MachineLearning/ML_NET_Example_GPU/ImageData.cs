111ddbc49db618b3c67a2cd84166907fe20f5510
Tomo21 <64405129+Tomo21@users.noreply.github.com>
2020-04-29 09:29:46 +0200

    ML.NET GPU Example

---
+++ MachineLearning/ML_NET_Example_GPU/ImageData.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.ML.Data;
+
+namespace ML_NET_Example_GPU
+{
+    public class ImageData
+    {
+        [LoadColumn(0)]
+        public byte[] Image { get; set; }
+
+        [LoadColumn(1)]
+        public string Label { get; set; }
+    }
+}
